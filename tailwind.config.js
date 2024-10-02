681e2d45406f37837ec0b4ef3b2361a3cf2fa075
Naufal Alif vivaldi <alifvivaldi726@gmail.com>
2024-10-02 13:44:00 +0700

    first

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      fontFamily : { customFont : [ '"font1"' , "prata" ] }
+    },
+  },
+  plugins: [],
+}
+
